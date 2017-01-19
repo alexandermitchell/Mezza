a4728fe621b7db5172405a3f6e4165aaabc2e513
Paul Jurczyk <pauljurczykny@gmail.com>
2017-01-19 14:23:31 -0500

    image picker works, just doesn’t look good

--- Mezza/Mezza/FeedItemDetailsViewController.swift
+++ Mezza/Mezza/FeedItemDetailsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FeedItemDetailsViewController: UIViewController {
+class FeedItemDetailsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     // MARK: Image Outlets --------------------------------------------------
     
@@ -24,12 +24,13 @@ class FeedItemDetailsViewController: UIViewController {
     
     @IBOutlet weak var imageView5: UIImageView!
     
-    // MARK: Remaingin Outlets ---------------------------------------------
+    // MARK: Remaining Outlets ---------------------------------------------
     
     @IBOutlet weak var productTitleLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var itemPriceLabel: UILabel!
     
+    @IBOutlet weak var itemSizesLabel: UILabel!
     // MARK: IBActions -----------------------------------------------------
     
     @IBAction func backButtonClicked(_ sender: UIButton) {
@@ -44,6 +45,51 @@ class FeedItemDetailsViewController: UIViewController {
     
     var selectedItem: Product?
     var currentSeller: User?
+    var sizeUIPicker: UIPickerView?
+    var sizesArray = [String]()
+    
+    // MARK: UIPicker Funcs ---------------------------------------------------------------
+    
+    // data method to return the numbe of columns in the picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    // data method to return the number of rows shown in the picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sizesArray.count
+    }
+    // delegate method to return the value shown in the picker
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sizesArray[row]
+    }
+    
+    // delegate method called when the row was selected.
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        itemSizesLabel.text = sizesArray[row]
+    }
+    
+    
+    func showSizePicker() {
+        // set up picker view
+        
+        sizeUIPicker = UIPickerView()
+        sizeUIPicker?.frame = CGRect(x: 0, y: self.view.layer.bounds.width, width: self.view.layer.bounds.height, height: 280.0)
+        sizeUIPicker?.delegate = self
+        sizeUIPicker?.dataSource = self
+        self.view.addSubview(sizeUIPicker!)
+    }
+    
+    // put all the product sizes into an array
+    
+    func createSizesArray() {
+        let sizesDict = selectedItem?.sizes
+        for size in sizesDict! {
+            let sizeString = size.name
+            sizesArray.append(sizeString)
+        }
+    }
+    
+    // MARK: VC Funcs ---------------------------------------------------------------------
     
     func putImagesInArray(closure: @escaping ([UIImage])->()) {
         var imagesArray = [UIImage]()
@@ -93,16 +139,24 @@ class FeedItemDetailsViewController: UIViewController {
         mainImageView.image = imageView5.image
     }
     
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createSizesArray()
         // fetch the current seller
         let sellerUID = selectedItem!.sellerUID
         DataModel.shared.fetchUser(UID: sellerUID) { seller in
             self.artistNameLabel.text = seller.name
         }
         
-        // setting up tap gesture recognizers
+        
+        // setting up tap gesture recognizer for size label
+        
+        itemSizesLabel.isUserInteractionEnabled = true
+        itemSizesLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(showSizePicker)))
+        
+        // setting up tap gesture recognizers for images
         let tapGestureRecognizer1 = UITapGestureRecognizer(target: self, action: #selector(changeMainImage1))
         let tapGestureRecognizer2 = UITapGestureRecognizer(target: self, action: #selector(changeMainImage2))
         let tapGestureRecognizer3 = UITapGestureRecognizer(target: self, action: #selector(changeMainImage3))
