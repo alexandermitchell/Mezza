fd6f7311ac38aaf9dd1b35c163d9a7202fc42fa1
Aman <amsingh714@gmail.com>
2017-01-20 14:07:07 -0500

    seller profile vc complete

--- Mezza/Mezza/SellerProfileViewController.swift
+++ Mezza/Mezza/SellerProfileViewController.swift
@@ -7,29 +7,49 @@
 //
 
 import UIKit
+import Firebase
 
 class SellerProfileViewController: UIViewController {
+    
+    @IBAction func SettingsButton(_ sender: Any) {
+        
+        
+    }
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var sellerName: UILabel!
+    @IBOutlet weak var sellerLocation: UILabel!
+    @IBOutlet weak var sellerDescription: UITextView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateSellerProfile()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    func updateSellerProfile() {
+        let loggedInUser = DataModel.shared.loggedInUser
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        loggedInUser?.name = sellerName.text!
+        loggedInUser?.location = sellerLocation.text!
+        loggedInUser?.bio = sellerDescription.text
+        
+        var seller: User?
+        
+        DataModel.shared.fetchImage(stringURL: (seller?.avatar)!) { (image) in
+            self.profileImage.image = image
+        }
     }
-    */
+    
 
 }
