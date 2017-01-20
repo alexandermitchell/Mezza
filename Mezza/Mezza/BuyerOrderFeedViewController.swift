bcd2ee30cba240e784c9ebaeffae20c71f4837ab
Aman <amsingh714@gmail.com>
2017-01-20 11:46:57 -0500

    added buyerorderfeed vc file

---
+++ Mezza/Mezza/BuyerOrderFeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BuyerOrderFeedViewController.swift
+//  Mezza
+//
+//  Created by Aman Singh on 1/20/17.
+//  Copyright © 2017 Alex Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class BuyerOrderFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
