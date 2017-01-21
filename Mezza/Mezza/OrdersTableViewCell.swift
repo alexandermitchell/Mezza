26d123104a0059b52d6db96359e86d5669b9a828
Paul Jurczyk <pauljurczykny@gmail.com>
2017-01-20 21:00:58 -0500

    tableview set up on the orders VC

---
+++ Mezza/Mezza/OrdersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrdersTableViewCell.swift
+//  Mezza
+//
+//  Created by Paul Jurczyk on 1/20/17.
+//  Copyright © 2017 Alex Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class OrdersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
